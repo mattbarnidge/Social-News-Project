767b238bc7ed3a65bfd7d273d09bd04014913142
mattbarnidge <mbarni109@gmail.com>
2022-10-11 17:02:04 -0500

    Update Analysis.R

--- Social News Project/Social News/Analysis.R
+++ Social News Project/Social News/Analysis.R
@@ -3,6 +3,17 @@ setwd("~/Documents/GitHub/Social-News-Project/Social News Project/Social News")
 load("socnews.Rdata")
 load("long.Rdata")
 
+#Mean Differences
+with(subset(d, fb.acct==1), 
+     cor(cbind(d$fb.freq, d$soccon, d$combel, 
+               d$soctrust, d$commtrust, d$newstrust, d$newstrust2, 
+               d$bustrust, d$govtrust, d$scitrust), 
+         use="complete.obs", method="pearson"))
+
+with(subset(d, fb.acct==1), 
+     cor(cbind(d$fbnews, d$soccon, d$combel), 
+         use="complete.obs", method="pearson"))
+
 #MLM
 library(lme4)
 library(lmerTest)
@@ -25,3 +36,9 @@ icc(m3)
 summary(m1, cor=FALSE)
 summary(m2, cor=FALSE)
 summary(m3, cor=FALSE)
+
+m4 = lmer(commtrust ~ agree + (1 + agree | ID), data=long, 
+          control=lmerControl(optimizer="bobyqa",
+                              optCtrl=list(maxfun=2e5)))
+summary(m4)
+
